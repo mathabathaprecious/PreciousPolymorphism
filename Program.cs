5bbd605a3266a6dfba0d2152f3c3e1e3c13af998
mathabathaprecious <mpuruprecious21@gmail.com>
2023-01-25 14:49:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using PreciousPolymorphism;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();  // Create a Animal object
+        Animal myPig = new Pig();  // Create a Pig object
+        Animal myDog = new Dog();  // Create a Dog object
+
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+    }
+}
